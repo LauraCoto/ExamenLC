b084071ec00fe97e6f794ed7d2ce1f6b2a879af9
Laura <laurahcoto@gmail.com>
2015-05-18 23:54:38 -0600

    Iniciando

---
+++ ExamenLC/ExamenLC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamenLC.Startup))]
+namespace ExamenLC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
